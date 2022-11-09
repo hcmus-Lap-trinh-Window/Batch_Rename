1ad1af51f371e88b88ae20fb18eb071d3bb3af73
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-11-09 22:05:50 +0700

    update

---
+++ AddPrefixRule/AddPrefixRule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommonModel
+{
+    public class AddPrefixRule : IRule
+    {
+        public string Name => throw new NotImplementedException();
+
+        public RuleStatus RuleState => throw new NotImplementedException();
+    }
+}
---
+++ TrimRule/TrimRule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommonModel
+{
+    public class TrimRule : IRule
+    {
+        public string Name => throw new NotImplementedException();
+
+        public RuleStatus RuleState => throw new NotImplementedException();
+    }
+}
