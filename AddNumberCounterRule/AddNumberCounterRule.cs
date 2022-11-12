ca70f16b04165e7983a96c9da6a8c899a7de9168
c0ngth4nk <congthanhtr>
2022-11-12 18:00:34 +0700

    WBR16: Add Counter Number Rule (at prefix) - logic tested, UI not tested yet

---
+++ AddNumberCounterRule/AddNumberCounterRule.cs
@@ -0,0 +1,93 @@
+using CommonModel;
+using RuleWindow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CommonModel
+{
+    public class AddNumberCounterRule : IRule
+    {
+        public string Name { get; set; }
+        public int Start { get;set; }
+        public int Step { get; set; }
+        public int NumOfDigits { get; set; }
+        public AddNumberCounterRuleWindow ConfigUI { get; set; }
+        public RuleStatus RuleState { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public AddNumberCounterRule()
+        {
+            this.Name = "Add Number Counter";
+            this.Step = 1;
+            this.Start = 1;
+            this.NumOfDigits = 0;
+            var instance = this;
+            this.ConfigUI = new AddNumberCounterRuleWindow(ref instance);
+        }
+
+        public string Apply(string originString, object parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> Apply(List<string> orginStringList, object parameters)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                if (parameters == null)
+                {
+                    throw new Exception("AddNumberCounterRule failed. Parameters cannot be null");
+                }
+                if (orginStringList != null && orginStringList.Count > 0)
+                {
+                    int indexOfFile = 0;
+                    foreach (var origin in orginStringList)
+                    {
+                        var currentCounter = this.Start + this.Step * indexOfFile;
+                        var counterPrefix = getCounterPrefix(currentCounter, NumOfDigits);
+                        if (!string.IsNullOrEmpty(counterPrefix))
+                        {
+                            var outputFileName = counterPrefix + origin;
+                            if (outputFileName.FileWithExtensionValidation(outputFileName.getExtension()))
+                            {
+                                result.Add(outputFileName);
+                            }
+                        }
+                        indexOfFile++;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException ?? ex);
+            }
+            return result;
+        }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+        /// <summary>
+        /// get counter prefix from current index and number of digits
+        /// </summary>
+        /// <param name="currentCounter"></param>
+        /// <param name="numofDigits"></param>
+        /// <returns></returns>
+        private static string getCounterPrefix(int currentCounter, int numofDigits)
+        {
+            string result = string.Empty;
+            var sCurrentCounter = currentCounter.ToString(); // string of current counter
+            if (sCurrentCounter.Length >= numofDigits)
+            {
+                result = currentCounter.ToString();
+            }
+            else
+            {
+                result = currentCounter.ToString().PadLeft(numofDigits, '0');
+            }
+            return result;
+        }
+    }
+}
---
+++ AddNumberCounterRule/AddNumberCounterRuleWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using CommonModel;
+using RuleWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RuleWindow
+{
+    /// <summary>
+    /// Interaction logic for AddNumberCounterRuleWindow.xaml
+    /// </summary>
+    public partial class AddNumberCounterRuleWindow : UserControl
+    {
+        AddNumberCounterRule rule;
+        public AddNumberCounterRuleWindow(ref AddNumberCounterRule rule)
+        {
+            this.rule = rule;
+            InitializeComponent();
+        }
+
+        private void startInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"[^0-9]+"); // check input must be a number
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void stepInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"[^0-9]+"); // check input must be a number
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void digitsInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"[^0-9]+"); // check input must be a number
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void startInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!startInput.Text.IsNullOrWhiteSpace())
+            {
+                this.rule.Start = int.Parse(startInput.Text);
+            }
+        }
+
+        private void stepInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!stepInput.Text.IsNullOrWhiteSpace())
+            {
+                this.rule.Step = int.Parse(stepInput.Text);
+            }
+        }
+
+        private void digitsInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!digitsInput.Text.IsNullOrWhiteSpace())
+            {
+                this.rule.NumOfDigits = int.Parse(digitsInput.Text);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = rule;
+        }
+    }
+}
