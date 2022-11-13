adfee58051cb3b1456326095b30fe64b7909bb96
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-11-13 22:59:45 +0700

    item WBR-6: gom converter vào chung folder converter

--- Batch_Rename_App/Converters/StatusColorConverter.cs
+++ Batch_Rename_App/Converters/StatusColorConverter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace Batch_Rename_App
+namespace Converter
 {
     internal class StatusColorConverter : IValueConverter
     {
