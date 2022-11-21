3f5b28683b8ceab8b95a40c69fe96b0c0425806f
tuan-khanh <nguyentuankhanhcqt@gmail.com>
2022-11-22 00:05:23 +0700

    WBR-17: Xóa tất cả khoảng trắng ở đầu và cuối file name.

---
+++ TrimRule/TrimRuleWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CommonModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RuleWindow
+{
+    /// <summary>
+    /// Interaction logic for TrimRuleWindow.xaml
+    /// </summary>
+    public partial class TrimRuleWindow : UserControl
+    {
+        TrimRule _rule;
+        public TrimRuleWindow(ref TrimRule trimRule)
+        {
+            this._rule = trimRule;
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+    public class ComparisonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value?.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value?.Equals(true) == true ? parameter : Binding.DoNothing;
+        }
+    }
+}
